6e9eee84504b48b9a0c430cd3fa3a838f8c52246
jye112 <jye112@naver.com>
2021-07-29 10:30:08 +0900

    linux, windows server output.tf modify

--- modules/linux-server/output.tf
+++ modules/linux-server/output.tf
@@ -1,9 +1,7 @@
 output "linux_vm_private_ip" {
-    count = var.linux_vm_num
     value = azurerm_linux_virtual_machine.linux_vm.private_ip_address[count.index]
 }
 
 output "linux_vm_public_ip" {
-    count = var.linux_vm_num
     value = azurerm_linux_virtual_machine.linux_vm.public_ip_address[count.index]
 }
--- modules/windows-server/output.tf
+++ modules/windows-server/output.tf
@@ -1,9 +1,7 @@
 output "windows_vm_private_ip" {
-    count = var.windows_vm_num
     value = azurerm_windows_virtual_machine.windows_vm.private_ip_address[count.index]
 }
 
 output "windows_vm_public_ip" {
-    count = var.windows_vm_num
     value = azurerm_windows_virtual_machine.windows_vm.public_ip_address[count.index]
 }
