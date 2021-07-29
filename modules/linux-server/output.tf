ba834199463f9e013982d1bc98f637080f888894
jye112 <jye112@naver.com>
2021-07-29 10:39:12 +0900

    modify

--- modules/linux-server/output.tf
+++ modules/linux-server/output.tf
@@ -1,7 +1,7 @@
 output "linux_vm_private_ip" {
-    value = azurerm_linux_virtual_machine.linux_vm[count.index].private_ip_address
+    value = azurerm_linux_virtual_machine.linux_vm[var.linux_vm_num.index].private_ip_address
 }
 
 output "linux_vm_public_ip" {
-    value = azurerm_linux_virtual_machine.linux_vm[count.index].public_ip_address
+    value = azurerm_linux_virtual_machine.linux_vm[var.linux_vm_num.index].public_ip_address
 }
