76146f87883c98a8674242862904c06546fa6273
jye112 <50107548+jye112@users.noreply.github.com>
2021-09-08 15:02:13 +0900

    Update output.tf

--- modules/linux-server/output.tf
+++ modules/linux-server/output.tf
@@ -1,10 +1,10 @@
-# output "linux_vm_private_ip" {
-#     value = azurerm_linux_virtual_machine.linux_vm.private_ip_address
-# }
+output "linux_vm_private_ip" {
+    value = azurerm_linux_virtual_machine.linux_vm.*.private_ip_address
+}
 
-# output "linux_vm_public_ip" {
-#     value = azurerm_linux_virtual_machine.linux_vm.public_ip_address
-# }
+output "linux_vm_public_ip" {
+    value = azurerm_linux_virtual_machine.linux_vm.*.public_ip_address
+}
 
 output "linux_vm_nic" {
     value = azurerm_linux_virtual_machine.linux_vm.*.network_interface_id
