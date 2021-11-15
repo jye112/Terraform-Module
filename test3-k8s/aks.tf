efae102e908259ef8ae794efb8a9366ec0d53d0d
jye112 <50107548+jye112@users.noreply.github.com>
2021-11-15 09:34:45 +0900

    Update aks.tf

--- test3-k8s/aks.tf
+++ test3-k8s/aks.tf
@@ -6,6 +6,7 @@ resource "azurerm_resource_group" "rg" {
 module "network" {
   source                    = "../modules/network"
   resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
   vnet_name                 = "aks-vnet"
   vnet_address_space        = "10.0.0.0/8"
   subnet_name               = ["aksSubnet01", "subnet01"]
