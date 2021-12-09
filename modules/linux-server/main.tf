e45a83aea538b4dc2471482486ef7d3e3c4bfb90
jye112 <50107548+jye112@users.noreply.github.com>
2021-12-09 10:55:10 +0900

    Update main.tf

--- modules/linux-server/main.tf
+++ modules/linux-server/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_network_interface" "linux_vm_nic" {
     name                          = var.ip_configuration_name
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = var.private_ip_address_allocation
-    public_ip_address_id          = ["${azurerm_public_ip.pip[count.index].id"]
+    public_ip_address_id          = ["${azurerm_public_ip.pip[count.index].id}"]
   }
 }
 
