6b4f01dbf1138a9064bb89a1b4ce389c9fe523cf
jye112 <50107548+jye112@users.noreply.github.com>
2021-12-09 11:02:32 +0900

    Update output.tf

--- modules/pip/output.tf
+++ modules/pip/output.tf
@@ -2,10 +2,6 @@ output "public_ip_address_id" {
   value = azurerm_public_ip.pip.*.id
 }
 
-output "public_ip_num" {
-  value = azurerm_public_ip.pip.count
-}
-
 output "public_ip_name" {
   value = azurerm_public_ip.pip.name
 }
