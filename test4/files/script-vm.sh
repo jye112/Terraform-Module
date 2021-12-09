0d9139a0f3589fd81f96e3c4f88cb6b6b270d94a
jye112 <50107548+jye112@users.noreply.github.com>
2021-12-09 09:41:44 +0900

    Create script-vm.sh

---
+++ test4/files/script-vm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt -y update 
+sleep 15 
+sudo apt -y update 
+sudo apt -y install apache2 
+sudo systemctl start apache2 
+sudo chmod 666 /etc/apache2/ports.conf 
+sudo chmod 666 /etc/apache2/sites-available/000-default.conf 
+sudo sed -i 's/80/8080/g' /etc/apache2/ports.conf 
+sudo sed -i 's/80/8080/g' /etc/apache2/sites-available/000-default.conf 
+sudo systemctl restart apache2 
+sudo chown -R ${var.cloo_vm_admin_username}:${var.cloo_vm_admin_username} /var/www/html 
+
+cat << EOM > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Hanwha Solution Terraform PoC</h1>
+<p>VM2 Success!!</p>
+</body>
+</html>
+EOM
